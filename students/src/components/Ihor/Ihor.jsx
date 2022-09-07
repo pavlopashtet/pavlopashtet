8fc266ca59b2b438e86cdeba4966e57033d05845
IhorKurylov <idSNrY7I3xhB>
2022-09-07 20:30:29 +0300

    clean

--- students/src/components/Ihor/Ihor.jsx
+++
@@ -1,9 +0,0 @@
-
-const IhorComponent = ()=>{
-    console.log("Ihor is here")
-    return(
-        <h1>Ihor Component</h1>
-    )
-}
-
-export default IhorComponent
--- students/src/components/Sasha/Sasha.js
+++
@@ -1,8 +0,0 @@
-const Sasha = () => {
-    console.log("Sasha is here")
-    return(
-        <h1>Sasha component</h1>
-    )
-}
-
-export default Sasha;
