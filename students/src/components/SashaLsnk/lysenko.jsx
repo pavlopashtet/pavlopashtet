fca524b26864ffc0c51b487f51aeb48f9558de78
Sasha <olysenko25@gmail.com>
2022-09-12 15:19:36 +0300

    created new component

--- students/src/components/SashaLsnk/lysenko.jsx
+++ students/src/components/SashaLsnk/lysenko.jsx
@@ -1,9 +1,30 @@
-const SashaComp = () => {
-    console.log("Sasha is here");
+import React from "react";
+import { useState } from "react";
 
+const SashaComp = () => {
+    const[isActive, setIsActive] = useState("true"); 
+
+    const handleClick = () => {
+        setIsActive(current => !current);
+    };  
 
     return(
-        <h1>Sasha Component</h1>
+        <div> 
+        <div 
+        style={{
+            backgroundColor: isActive ? "pink" : "",
+            color: isActive ? "AliceBlue" : "",
+        }}
+        >
+        
+        <h1>Sasha's Component</h1>
+        <button onClick={handleClick}> 
+        
+        {isActive ? "Where is Sasha?" : "Sasha is here"}
+
+        </button>
+        </div>
+        </div>
     )
 }
 
