a4b5ff5e00d65eaa2e6af22881ca93b3332ea732
Vitalii1813 <packun.v@gmail.com>
2022-09-20 11:20:48 +0300

    Vitalii done

--- students/src/pages/Vitalii/Vitalii.jsx
+++ students/src/pages/Vitalii/Vitalii.jsx
@@ -3,7 +3,7 @@ const Vitalii = ()=> {
     const [action, setAction] = useState("true");
     return (
         <div>
-            <button onClick={() => setAction(!action)}> {action ? 'Vitalii' : 'My name is Vitalii, at the moment I am interested in web development. In addition to frontend, I like sports, namely football and modern dance.'}
+            <button onClick={() => setAction(!action)}> {action ? 'Vitalii' : 'My name is Vitalii, at the moment I am interested in web development.'}
             </button>
         </div>
     );
