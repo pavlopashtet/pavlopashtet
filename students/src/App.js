8a61f5fbadb38e929e877e56d0de563a6f65936b
Vitalii1813 <packun.v@gmail.com>
2022-09-20 15:47:26 +0300

    Job done

--- students/src/App.js
+++ students/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from "./components/header/Header";
 import { Route, Routes } from "react-router-dom";
-import MentorContainerPage from "./pages/Mentor/MentorContainerPage";
 import NotFoundPAge from "./pages/404/NotFoundPAge";
 import IhorComponent from "./pages/Ihor/Ihor";
 import { AppRoutes } from "./common/Routes";
@@ -20,21 +19,16 @@ const App = () => {
     console.log(authUser);
     return (
         <>
-            <Header/>
+            <Header />
             <Routes>
-                <Route path={AppRoutes.MENTOR} element={<MentorContainerPage />}/>
-
-
-
-                <Route path={AppRoutes.MAIN} element={<h1>Main</h1>}/>
-                {/*<Route path={AppRoutes.HOC} element={<HOC Component={<h1>COMPONENT argument</h1>} a={12}/>}/>*/}
-                {/*<Route path={AppRoutes.MENTOR} element={*/}
-                {/*    <MentorRoute authUser={authUser} Component={MentorContainerPage}/>*/}
-                {/*}/>*/}
-                <Route path={AppRoutes.NOT_FOUND} element={<NotFoundPAge/>}/>
                 <Route path={AppRoutes.Ihor} element={<IhorComponent/>}/>
+                <Route path="/404" element={<NotFoundPAge/>}/>
                 <Route path="*" element={<NotFoundPAge/>}/>
             </Routes>
+            <Vitalii/>
+            {/*<Miki/>*/}
+            {/*<Iryna/>*/}
+            {/*<SashaComp/>*/}
         </>
     );
 }
@@ -47,5 +41,5 @@ export default App;
 // {/*<Miki/>*/}
 // {/*<Iryna/>*/}
 // {/*<SashaComp/>*/}
-<Vitalii/>
+
 
