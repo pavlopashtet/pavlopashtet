b0941abb568f775cafbd5ec5ee88e03e41fd35a0
IhorKurylov <idSNrY7I3xhB>
2022-09-27 08:36:38 +0300

    consult

--- students/src/common/Routes.js
+++ students/src/common/Routes.js
@@ -31,7 +31,6 @@ export const RedirectRoutes = {
     Sasha: "/sasha",
     Iryna:"/Iryna",
     CV:"/cv",
-    MainComponent:"/liftState",
-    NATALIA: "/natalia"
+    MainComponent:"/liftState"
 }
 
