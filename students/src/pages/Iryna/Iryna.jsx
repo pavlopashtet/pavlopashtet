6ce38cf663b7d473290561cfa86629df525b8233
Bedarska-Iryna <bednarska.ira.2000@gmail.com>
2022-10-02 16:14:28 +0300

    Form State done

--- students/src/pages/Iryna/Iryna.jsx
+++ students/src/pages/Iryna/Iryna.jsx
@@ -4,50 +4,50 @@ import { AppRoutes, RedirectRoutes } from "../../common/Routes";
 import Con from "./CV/CV";
 import { Link } from "react-router-dom";
 import MainComponent from "./liftState/liftState";
-// import NotFoundPAge from "../404/NotFoundPAge";
 import styles from "./Iryna.module.css";
 import InputState from "./inputState/inputState";
-import FormState from "./formState/formState";
+import MainForm from "./formState/MainForm";
 
 const Iryna = () => {
   const [action, setAction] = useState("true");
   return (
-    <div className={styles.Main}>
-
-<button
-        style={{ margin: "50px 800px", width: "300px", height: "150px", backgroundColor:"deeppink", color:"white"}}
+    <div className={styles.Main}>      
+      <button
+        style={{
+          margin: "50px 800px",
+          width: "300px",
+          height: "150px",
+          backgroundColor: "deeppink",
+          color: "white",
+        }}
         onClick={() => setAction(!action)}
       >
         {" "}
         {action
           ? "Iryna"
           : "My name is Iryna, at the moment I am interested in web development. In addition to frontend, I like sports, namely football and modern dance."}
-      </button>  
+      </button>
       <Link to={`${RedirectRoutes.Iryna}${AppRoutes.CV}`}>
         <span style={{ fontSize: "50px" }}>CV</span>
-      </Link>    
+      </Link>
       <Link to={`${RedirectRoutes.Iryna}${AppRoutes.MainComponent}`}>
         <span style={{ fontSize: "50px" }}>liftState</span>
-      </Link>  
+      </Link>
       <Link to={`${RedirectRoutes.Iryna}${AppRoutes.InputState}`}>
         <span style={{ fontSize: "50px" }}>InputState</span>
-      </Link> 
+      </Link>
       <Link to={`${RedirectRoutes.Iryna}${AppRoutes.FormState}`}>
         <span style={{ fontSize: "50px" }}>FormState</span>
-      </Link> 
+      </Link>
       <Routes>
-        <Route path={AppRoutes.CV} element={<Con/>} />
-        <Route path={AppRoutes.MainComponent} element={<MainComponent/>} />
-        <Route path={AppRoutes.InputState} element={<InputState/>} />
-        <Route path={AppRoutes.FormState} element={<FormState/>} />
+        <Route path={AppRoutes.CV} element={<Con />} />
+        <Route path={AppRoutes.MainComponent} element={<MainComponent />} />
+        <Route path={AppRoutes.InputState} element={<InputState />} />
+        <Route path={AppRoutes.FormState}  element={<MainForm/>
+          }
+        />
       </Routes>
-
-
     </div>
-    
-
-    
-    
   );
 };
 export default Iryna;
--- students/src/pages/Iryna/formState/formState.jsx
+++ students/src/pages/Iryna/formState/formState.jsx
@@ -1,78 +1,32 @@
 import styles from "../formState/formState.module.css";
-import React from "react";
+import { React, useState } from "react";
 
-export const Bn = ({bn}) => {
-    <div className={styles.box}>{Bn}</div>
-}
-// const initialFormData = Object.freeze({
-//     usertext: "",
-    
-//   });
-//   const FormState = () => {
-//     const [formData, updateFormData] = React.useState(initialFormData);
-  
-//     const handleChange = (e) => {
-//       updateFormData({
-//         ...formData,
-       
-//       });
-//     };
-  
-//     const handleSubmit = (e) => {
-//       e.preventDefault()
-//       console.log(formData);
-//       // ... submit to API or something
-//     };
-  
-//     return (
-//       <>
-//         <label>
-//           Usertext
-//           <input name="username" onChange={handleChange} />
-//         </label>      
-//         <br />
-//         <button onClick={handleSubmit}>Submit</button>
-//       </>
-//     );
-//   };
-
-
-class FormState extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+const FormState = ({ setFormValue, setEdit}) => {
+  const [inputValue, setInputValue] = useState({ text: "" });
+  const handleButtonClick = () => {
+    setFormValue(inputValue)
+    setEdit((prevState) => !prevState)
   }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    // <Bn bn={this.state.value}/>
-    console.log(this.state.value);
-    alert("Введіть текст" + this.state.value)
-    // <div className={styles.box}>{this.state.value}</div>;
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <>
-        <form  onSubmit={this.handleSubmit}>
-          <label>
-            Введіть текст:
-            <input name ="usertext" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          
-        </form>
-        <button onClick={this.handleSubmit}>Submit</button>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <form>
+        <label>
+          Введіть текст: 
+          <br></br>
+          <br></br>        
+          <textarea
+            value={inputValue.text}
+            onChange={(e) =>
+              setInputValue({ 
+                ...inputValue, 
+                text: e.target.value })
+            }
+          />
+        </label>
+      </form>      
+      <button className = {styles.btn} onClick={handleButtonClick}> Submit</button>
+    </>
+  );
+};
 
 export default FormState;
