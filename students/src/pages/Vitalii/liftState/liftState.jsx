a3b745e3804d2af5955fc460f71da8fb617536c7
Vitalii1813 <packun.v@gmail.com>
2022-10-03 18:53:25 +0300

    Merge pull request #20 from pavlopashtet/Vitalii
    Vitalii

---
+++ students/src/pages/Vitalii/liftState/liftState.jsx
@@ -0,0 +1,15 @@
+
+import styles from "./liftState.module.css";
+import {React} from "react";
+
+
+const State = () => {
+
+    return (
+        <div className={styles.container}>
+           <button>Color1</button>
+            <button>Color2</button>
+        </div>
+    );
+};
+export default State;
