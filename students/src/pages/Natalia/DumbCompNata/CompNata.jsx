7333739dac5682e1ece9ec80184ac7252407e122
Vitalii1813 <packun.v@gmail.com>
2022-10-13 14:01:23 +0300

    Merge pull request #20 from pavlopashtet/Vitalii
    Vitalii

--- students/src/pages/Natalia/DumbCompNata/CompNata.jsx
+++ students/src/pages/Natalia/DumbCompNata/CompNata.jsx
@@ -1,24 +1,18 @@
 import React from "react";
-import OneBtn from "./OneBtn";
+import OneBtn from "./BtnOne";
 import styles from "../../Natalia/DumbCompNata/CompNata.module.scss";
-import TwoBtn from "./Two.Btn";
+import TwoBtn from "./BtnTwo";
 
 
 const NataComp = () => {
     const [hex,setHex] = useState("#Box");
-    const handleOneBtn = () => {
-        setHex(false)
-    }
-
     return(
         <>
             <div className={styles.box} style={{backgroundColor: hex}}></div>
-            <BtnOne
+            <OneBtn
                 setHex={setHex}
            />
-            <BtnTwo
-                // setHex={setHex}
-                // hex = {hex}
+            <TwoBtn
                 handelClick= { () => hex === "red" ? setHex("purple") :setHex("orange") }
             />
         </>
@@ -26,35 +20,3 @@ const NataComp = () => {
 }
 
 export default NataComp;
-
-
-
-
-
-// const NataComp = () => {
-//     // const [color,setColor] = useState('#000');
-//     // const [bgr, setBgr] = useState('#000')
-//
-//     // const handelClick = () => {
-//     //     const color = {
-//     //         r: getRgb(),
-//     //         g: getRgb(),
-//     //         b: getRgb()
-//     //
-//     //     };
-//     //     setColor(rgbToHex(color.r,color.g,color.b,))
-//
-//      return(
-//          <>
-//          <div className={styles.box}><h1>BOX</h1></div>
-//              <OneBtn
-//                  // setBgr = {(setBgr)}
-//              />
-//              <TwoBtn />
-// </>
-//      )
-//          }
-//
-//
-//
-// export default NataComp;
