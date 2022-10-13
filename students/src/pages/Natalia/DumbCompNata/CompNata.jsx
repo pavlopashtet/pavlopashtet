364e8f5962821c1e946900e6891c20de8c4a303b
Vitalii1813 <packun.v@gmail.com>
2022-10-13 14:03:25 +0300

    Merge pull request #20 from pavlopashtet/Vitalii
    Vitalii

--- students/src/pages/Natalia/DumbCompNata/CompNata.jsx
+++ students/src/pages/Natalia/DumbCompNata/CompNata.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React,useState} from "react";
 import OneBtn from "./BtnOne";
 import styles from "../../Natalia/DumbCompNata/CompNata.module.scss";
 import TwoBtn from "./BtnTwo";
@@ -6,6 +6,10 @@ import TwoBtn from "./BtnTwo";
 
 const NataComp = () => {
     const [hex,setHex] = useState("#Box");
+    // const handleOneBtn = () => {
+    //     setHex(false)
+    // }
+
     return(
         <>
             <div className={styles.box} style={{backgroundColor: hex}}></div>
