339b388f194b32c3e65f9e72866771d340252f75
NataliaTymkiv <Natalia1987989@gmail.com>
2022-10-17 17:33:49 +0300

    Natalia done

--- students/src/pages/Mentor/forms/Forms.js
+++ students/src/pages/Mentor/forms/Forms.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import
+{ useEffect, useState } from "react";
 import { getCharacter, postPostToPlaceholder } from "../../../api/apiCalls";
 
 const initialData = {
