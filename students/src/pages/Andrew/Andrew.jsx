cdabb1e504aeef6fea2177dbd30f50672f93bfca
AndrewPenzovskiy <karabiner.ml@gmail.com>
2022-10-19 14:45:06 +0300

    commit 1

--- students/src/pages/Andrew/Andrew.jsx
+++ students/src/pages/Andrew/Andrew.jsx
@@ -1,15 +1,14 @@
+import s from './Andrew.module.css'
 import React from "react";
-import {Route, Routes} from "react-router-dom";
-import {AppRoutes} from "../../common/Routes";
+import Sidebar from "./sidebar/Sidebar";
+import Content from "./content/Content";
+
 
 const AndrewComponent = ()=>{
     return(
-        <div>
-        <h1>Andrew's Component</h1>
-
-    <Routes>
-        <Route path={AppRoutes.Andrew} element={<h2>CV</h2>}/>
-    </Routes>
+        <div className={s.container}>
+            <Sidebar />
+            <Content />
         </div>
     )
 }
