78a8447a74a5faaeed7fa0c1dbf8d6add53d2ddc
NATALIIA <nkoziar@ukr.net>
2022-10-20 14:17:39 +0300

    axios done

--- students/src/pages/NataliaKoziar/natalkaComponents/axios/AxiosComponent.jsx
+++ students/src/pages/NataliaKoziar/natalkaComponents/axios/AxiosComponent.jsx
@@ -13,9 +13,10 @@ const instance2 = axios.create({
 
 export const AxiosComponent = () => {
     const [item, setItem] = useState({})
-    const [city, setCity] = useState('')
+    const [city, setCity] = useState('lviv')
     const [searchCity, setSearchCity] = useState('')
     const [shoveMore, setShowMore] = useState(false)
+    const [isLoading, setLoading] = useState()
 
     const getWeather = (url = "lviv") => {
         return instance2.get(url)
@@ -26,59 +27,56 @@ export const AxiosComponent = () => {
         setShowMore(false)
     }
 
-    useEffect(() => {
-        console.log("render");
-       getWeather()
-            .then((response) => {
-                console.log(response.data);
-                setItem(response.data)
-                
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+    // useEffect(() => {
+    //     console.log("render");
+    //    getWeather()
+    //         .then((response) => {
+    //             console.log(response.data);
+    //             setItem(response.data)
 
-    }, [])
-    useEffect(()=>{
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         })
+
+    // }, [])
+    useEffect(() => {
         console.log("render2");
+        setLoading(true)
         getWeather(city)
             .then((response) => {
                 console.log(response.data);
                 setItem(response.data)
-                console.log(item);
             })
             .catch((err) => {
                 console.log(err);
             })
-    },[city])
+            .finally(() => setLoading(false))
+    }, [city])
 
     return (
         <div className={s.weather}>
             <input type="text" value={searchCity} onChange={(e) => setSearchCity(e.target.value)} placeholder='Choose city...' />
             <button onClick={getSearch}>Show the weather</button>
             <div className={s.description}>
-                <h5>{item.region}</h5>
-               <h6>{item.currentConditions?.dayhour}</h6>
-               <img width={'80px'} src= {item.currentConditions?.iconURL} alt='icon'/>
-               <p>{item.currentConditions?.comment}</p>
-               <p className={s.temp}>{item.currentConditions?.temp.c}ºC</p>
-               <p>humidity: {item.currentConditions?.humidity}</p>
-               <p>precip: {item.currentConditions?.precip}</p>
-               <p>wind: {(item.currentConditions?.wind.km/3,6).toFixed(1)} m/s</p>
-                {/* <h5>Lviv</h5>
-                <h6>Wednesday</h6>
-                <img width={'80px'} src='https://sinst.fwdcdn.com/img/weatherImg/m/d320.gif' alt='icon' />
-                <p>Cloudy</p>
-                <p className={s.temp}>12ºC</p>
-                <p>humidity: 92%</p>
-                <p>precip: 9%</p>
-                <p>wind: 14 km/h</p> */}
+                {isLoading && <h4>Loading ... </h4>}
+                {!isLoading && <>
+                    <h5>{item.region}</h5>
+                    <h6>{item.currentConditions?.dayhour}</h6>
+                    <img width={'80px'} src={item.currentConditions?.iconURL} alt='icon' />
+                    <p>{item.currentConditions?.comment}</p>
+                    <p className={s.temp}>{item.currentConditions?.temp.c}ºC</p>
+                    <p>humidity: {item.currentConditions?.humidity}</p>
+                    <p>precip: {item.currentConditions?.precip}</p>
+                    <p>wind: {(item.currentConditions?.wind.km / 3, 6).toFixed(1)} m/s</p>
+                </>}
             </div>
-            <button onClick={()=>setShowMore(prev=>!prev)}>
-            {shoveMore? 'Hide forecast' : "See for a week"}
+            <button onClick={() => setShowMore(prev => !prev)}>
+                {shoveMore ? 'Hide forecast' : "See for a week"}
             </button>
             <div className={s.weather_cards}>
-            {shoveMore && <WeatherCard/>}
+                {shoveMore && item.next_days?.filter((el, i) => i > 0)
+                    .map((element, i) => <WeatherCard key={i} data={element} />)}
             </div>
         </div>
     )
