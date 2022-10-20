e1d8f98e1868375bd7d02337e7bc9dcd71a8102c
NATALIIA <nkoziar@ukr.net>
2022-10-20 14:17:39 +0300

    axios done

--- students/src/pages/NataliaKoziar/natalkaComponents/axios/WeatherCard.jsx
+++ students/src/pages/NataliaKoziar/natalkaComponents/axios/WeatherCard.jsx
@@ -2,10 +2,14 @@ import s from "./AxiosComponent.module.scss"
 
 
 
-export const WeatherCard = ()=>{
+export const WeatherCard = ({data})=>{
     return(
         <div className={s.card}>
-            hello!
+            <h6>{data.day}</h6>
+            <img src= {data.iconURL} alt='icon'/>
+            <p>{data.comment}</p>
+            <p>max:{data.max_temp.c}ºC,</p>
+            <p>min:{data.min_temp.c}ºC</p>
         </div>
     )
 }
