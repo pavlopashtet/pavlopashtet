b2519c566dd73e0e53c9fe520725a6d43db3133e
Stepan Rudiak <stepan.rudiak95@gmail.com>
2022-10-23 17:58:46 +0300

    HomeWork redux 3
    HomeWork redux 2

--- students/src/pages/StepanRudiak/components/reduxSR/index.js
+++ students/src/pages/StepanRudiak/components/reduxSR/index.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {StepanActions} from "../../../../redux/action/StepanActions";
-
+import s from './reduxStyle.module.sass';
+import {stepGetCharactersOnInit, stepGetMoreCharacters} from "../../../../redux/action/StepanActions";
 const ReduxSr = () => {
   const dispatch = useDispatch();
-  const state = useSelector(state => state.Stepan);
-  const inputRef = React.createRef();
-
-  const incrementFn = () => dispatch(StepanActions.increment())
-  const decrementFn = () => dispatch(StepanActions.decrement())
-  const incrementByValFn = () => dispatch(StepanActions.incrementByVal(+inputRef.current.value))
-  const decrementByValFn = () => dispatch(StepanActions.decrementByVal(+inputRef.current.value))
+  const characters = useSelector(state => state.Stepan.characters);
+  const isLoading = useSelector(state => state.Stepan.loading);
+  const info = useSelector(state => state.Stepan.info);
+  const _stepGetCharactersOnInitThunk = () => dispatch(stepGetCharactersOnInit())
+  const _stepGetMoreCharactersThunk = (someInfo) => dispatch(stepGetMoreCharacters(someInfo))
+  useEffect(() =>{
+    _stepGetCharactersOnInitThunk()
+  }, []);
+  const getMore = () => {
+    _stepGetMoreCharactersThunk(info.next)
+  }
   return (
     <div>
-      <div>{state.counter}</div>
-      <div>
-        <input ref={inputRef} type="number" placeholder={'type a number you want to add or subtract'} defaultValue={10}/>
+      <div className={s.charactersContainer}>
+        {isLoading ?
+        <h1>Data is Loading... </h1>
+          :
+          characters.length > 0 && characters.map(item => (
+            <div key={item.id} className={s.charactersItem}>
+              <img src={item.image} alt=""/>
+              <p>{item.name}</p>
+            </div>
+          ))
+        }
       </div>
-      <div>hello</div>
-      <div>
-        <button onClick={incrementFn}>increment</button>
-        <button onClick={decrementFn}>decrement</button>
-        <button onClick={incrementByValFn}>increment by</button>
-        <button onClick={decrementByValFn}>decrement by</button>
-      </div>
+      {info?.next && (
+        <div>
+          <button onClick={getMore}>Add more</button>
+        </div>
+      )}
+
     </div>
   );
 };
